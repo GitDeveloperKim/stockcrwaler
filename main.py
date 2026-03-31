3c2860a4c0a33680f8915f564e8e7cda2563b54a
yskim <ergla88@gmail.com>
2026-03-31 23:44:53 +0900

    web crawling without chrome drvier

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
-def print_hi(name):
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
-    print(f'Hi, {name}')  # 중단점을 전환하려면 Ctrl+F8을(를) 누릅니다.
+# 브라우저 꺼짐 방지 옵션
+chrome_options = Options()
+chrome_options.add_experimental_option("detach", True)
 
-if __name__ == '__main__':
-    print_hi('PyCharm')
+# 드라이버 설정 (경로 입력 없이 바로 실행 가능!)
+driver = webdriver.Chrome(options=chrome_options)
 
+# 테스트 실행
+driver.get("https://www.google.com")
+print(driver.title)
